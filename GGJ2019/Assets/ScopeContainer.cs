1beb9cf7989bb417e79ad2c54faed1bc4252dc69
Amos Wazana <amoswazana@gmail.com>
2019-01-26 12:07:01 +0200

    Scope basic collider

---
+++ GGJ2019/Assets/ScopeContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeContainer : MonoBehaviour {
+
+    internal bool IsOverScope;
+
+    private void OnMouseEnter() {
+        IsOverScope = true;
+    }
+
+    private void OnMouseExit() {
+        IsOverScope = false;
+
+    }
+}
